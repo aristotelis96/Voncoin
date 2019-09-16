57e5b833edc7dd700c653bce741688692c67d9db
aristotelis96 <aristotelis96@users.noreply.github.com>
2019-09-16 14:31:24 +0300

    added node class. Only register works for now

--- tools.py
+++ tools.py
@@ -1,4 +1,7 @@
 import socket
+import blockchain as blockchainModule
+import block
+ 
 def get_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
@@ -9,3 +12,21 @@ def get_ip():
     finally:
         s.close()
     return IP
+
+def create_chain_from_dump(chain_dump):
+    newblockchain = blockchainModule.Blockchain()
+    for idx, block_data in enumerate(chain_dump):
+        newblock = block.Block(block_data["index"],
+                               block_data["previous_hash"],
+                               block_data["transactions"])
+        newblock.timestamp = block_data["timestamp"]
+        proof = block_data['hash']
+        if idx > 0:
+            newblock.nonce = block_data["nonce"]
+            added = newblockchain.add_block(newblock, proof)
+            if not added:
+                raise Exception("The chain dump is tampered!!")
+        else:  # else the block is a genesis block, no verification needed
+            newblock.hash = proof
+            newblockchain.chain.append(newblock)
+    return newblockchain
